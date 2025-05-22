c76dccbdd471410099d8dae00e7da6b49418f983
BaiLap4 <Phucgp74@gmail.com>
2025-05-22 09:50:06 +0700

    Section1

---
+++ TH01/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TH01.Models.Interface;
+
+namespace TH01.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public ActionResult Shop() 
+        {
+            return View(productRepository.GetAllProducts());
+        }
+    }
+}
---
+++ TH01/Models/Interface/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace TH01.Models.Interface
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        IEnumerable<Product> GetTrendingProducts();
+        Product GetProductDetail(int id);
+    }
+}
